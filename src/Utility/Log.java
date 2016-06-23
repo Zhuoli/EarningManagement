0135c79beaf036d639209cefc4fce20f86a7049a
Zhuoli <zhuoliseattle@gmail.com>
2016-06-23 01:00:33 -0700

    Replace old code

---
+++ src/Utility/Log.java
@@ -0,0 +1,11 @@
+package src.Utility;
+
+/**
+ * Created by zhuoli on 6/23/16.
+ */
+public class Log {
+
+    public static void PrintAndLog(String message) {
+        System.out.println(message);
+    }
+}
