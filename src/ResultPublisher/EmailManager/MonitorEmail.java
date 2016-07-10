5d110b0ddd5851926bcadec5ff04f3b89dded54e
Zhuoli <zhuoliseattle@gmail.com>
2016-07-10 01:35:15 -0700

    Email send, receive works now

---
+++ src/ResultPublisher/EmailManager/MonitorEmail.java
@@ -0,0 +1,25 @@
+package src.ResultPublisher.EmailManager;
+
+/**
+ * Created by zhuoli on 7/10/16.
+ */
+public class MonitorEmail {
+
+    public String Folder;
+
+    public String Subject;
+
+    public String Recipient;
+
+    public String From;
+
+    public String Content;
+
+    public MonitorEmail(String folder, String subject, String recipient, String from, String content) {
+        this.Folder = folder;
+        this.Subject = subject;
+        this.Recipient = recipient;
+        this.From = from;
+        this.Content = content;
+    }
+}
