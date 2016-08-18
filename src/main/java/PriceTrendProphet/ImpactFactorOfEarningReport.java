4b1b00f324aa05f4bdeab62ce148e16a83ce938b
zhuolil <zhuoliseattle@gmail.com>
2016-08-18 00:05:03 -0700

    Add ImpactFactor

---
+++ src/main/java/PriceTrendProphet/ImpactFactorOfEarningReport.java
@@ -0,0 +1,9 @@
+package PriceTrendProphet;
+
+/**
+ * Created by zhuoli on 8/13/16.
+ */
+public class ImpactFactorOfEarningReport {
+
+    final String EarningCalendarUrl = "http://www.nasdaq.com/earnings/earnings-calendar.aspx";
+}
