55477dcdbaed78ecfc79f9bfcc4030140ba5d041
zhuolil <zhuoliseattle@gmail.com>
2016-08-18 00:25:07 -0700

    Add ImpactFactor

--- src/main/java/DataManager/DatabaseManager.java
+++ src/main/java/DataManager/DatabaseManager.java
@@ -1,4 +1,5 @@
 package DataManager;
+
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
@@ -6,7 +7,9 @@ import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
@@ -26,20 +29,11 @@ import java.util.logging.Logger;
 public class DatabaseManager extends DataManager{
 
 
-    static final public String SYMBOL = "Symbol";
-    static final public String SHARECOST = "ShareCost";
-    static final public String PRICE = "Price";
-    static final public String SHARES = "Shares";
-    static final public String REPORTDATE = "EarningReportDatetime";
-    private String url;
-
-    private String databaseString;
-
-    private String userName;
-
-    private String password;
-
     MysqlDataSource dataSource;
+    private String url;
+    private String databaseString;
+    private String userName;
+    private String password;
 
     private DatabaseManager() {
 
@@ -55,90 +49,6 @@ public class DatabaseManager extends DataManager{
 
     public static void main(String[] args) {
 
-    }
-
-    public DatabaseManager Authenticate() throws SQLException {
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-
-        this.dataSource = new MysqlDataSource();
-        this.dataSource.setUser(this.userName);
-        this.dataSource.setPassword(this.password);
-        this.dataSource.setServerName(this.url);
-        this.dataSource.setDatabaseName(this.databaseString);
-
-        try {
-            conn = dataSource.getConnection();
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT * FROM StockItem");
-            return this;
-
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            try {
-                stmt.close();
-                conn.close();
-                rs.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            };
-        }
-    }
-
-    @Override
-    public List<JSONObject> UpdateStockItemsIfHasNew()
-    {
-        try {
-            return this.ReadStockDatabase();
-        }
-        catch (SQLException e)
-        {
-            Logger.getGlobal().log(Level.SEVERE, "SQL Failure", e);
-        }
-        return new LinkedList<>();
-    }
-
-    /**
-     * Read stocks from CSV file.
-     * @return stock item array.
-     * @throws IOException
-     */
-    public List<JSONObject> ReadStockDatabase() throws SQLException {
-
-        LinkedList<JSONObject> jsonObjectList = new LinkedList<>();
-
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-
-        try {
-            conn = dataSource.getConnection();
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT * FROM StockItem");
-            while (rs.next())
-            {
-                String symbol = rs.getString(DatabaseManager.SYMBOL);
-                double price = rs.getDouble(DatabaseManager.PRICE);
-                int number = rs.getInt(DatabaseManager.SHARES);
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put(CSVDataManager.SYMBOL, symbol).put(CSVDataManager.PRICE, price).put(CSVDataManager.SHARES, number);
-                jsonObjectList.add(jsonObject);
-
-            }
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            try {
-                stmt.close();
-                conn.close();
-                rs.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            };
-        }
-        return jsonObjectList;
     }
 
     /**
@@ -167,7 +77,7 @@ public class DatabaseManager extends DataManager{
                 String user = databasesNode.getElementsByTagName("User").item(0).getTextContent();
                 String password = databasesNode.getElementsByTagName("Password").item(0).getTextContent();
 
-                return new DatabaseManager(url, database, user,password);
+                return new DatabaseManager(url, database, user, password);
             } catch (Exception e) {
                 Logger.getGlobal().log(Level.SEVERE, "Failed to read configuration file from " + pathString, e);
             }
@@ -175,5 +85,86 @@ public class DatabaseManager extends DataManager{
         } else {
             return new DatabaseManager();
         }
+    }
+
+    public DatabaseManager Authenticate() throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        this.dataSource = new MysqlDataSource();
+        this.dataSource.setUser(this.userName);
+        this.dataSource.setPassword(this.password);
+        this.dataSource.setServerName(this.url);
+        this.dataSource.setDatabaseName(this.databaseString);
+
+        try {
+            conn = dataSource.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM StockItem");
+            return this;
+
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            try {
+                stmt.close();
+                conn.close();
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public List<JSONObject> UpdateStockItemsIfHasNew()
+    {
+        try {
+            return this.ReadStockDatabase();
+        } catch (SQLException e) {
+            Logger.getGlobal().log(Level.SEVERE, "SQL Failure", e);
+        }
+        return new LinkedList<>();
+    }
+
+    /**
+     * Read stocks from CSV file.
+     * @return stock item array.
+     * @throws IOException
+     */
+    public List<JSONObject> ReadStockDatabase() throws SQLException {
+
+        LinkedList<JSONObject> jsonObjectList = new LinkedList<>();
+
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = dataSource.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM StockItem");
+            while (rs.next()) {
+                String symbol = rs.getString(DatabaseManager.SYMBOL);
+                double price = rs.getDouble(DatabaseManager.AVERAGECOST);
+                int number = rs.getInt(DatabaseManager.SHARES);
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put(DataManager.SYMBOL, symbol).put(DataManager.AVERAGECOST, price).put(DataManager.SHARES, number);
+                jsonObjectList.add(jsonObject);
+
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            try {
+                stmt.close();
+                conn.close();
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonObjectList;
     }
 }
