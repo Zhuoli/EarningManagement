c0776239cc70ec807c31ebbb45907af5920a55e9
zhuolil <zhuoliseattle@gmail.com>
2016-08-18 23:52:08 -0700

    Add oneyear target price

---
+++ src/main/java/DataManager/StockDatabase/SharedStockItems.java
@@ -0,0 +1,13 @@
+package DataManager.StockDatabase;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by zhuolil on 8/18/16.
+ */
+@Entity
+@Table(name="SharedStockItems")
+public class SharedStockItems {
+
+}
