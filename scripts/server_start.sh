6cbbd7f8b3ee24384f72dc63566fbe6aad15e854
Zhuoli <zhuoliseattle@gmail.com>
2016-10-13 00:47:25 -0700

    Configure to execute the deployment - Resoulve the path problem

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,13 @@
-#/bin/sh
-echo "Current Path:"
-pwd
-# cd target
-java -jar target/EarningManagement-1.0-SNAPSHOT-jar-with-dependencies.jar
+#!/bin/sh
+currentDir=$(
+  cd $(dirname "$0")
+  pwd
+)
+
+echo script $currentDir
+echo "target Path:"
+targetDir="$currentDir/../target"
+echo ${targetDir}
+cd $targetDir
+
+java -jar EarningManagement-1.0-SNAPSHOT-jar-with-dependencies.jar
