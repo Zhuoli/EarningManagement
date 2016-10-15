38136aebb1c082ad46345cddd72e0688e973e27a
Zhuoli <zhuoliseattle@gmail.com>
2016-10-15 02:33:25 -0700

    Commit out exit with 1

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -12,4 +12,5 @@ java -version
 echo ${targetDir}
 cd $targetDir
 
+unzip -p EarningManagement-1.0-SNAPSHOT-jar-with-dependencies.jar | head
 java -jar EarningManagement-1.0-SNAPSHOT-jar-with-dependencies.jar
